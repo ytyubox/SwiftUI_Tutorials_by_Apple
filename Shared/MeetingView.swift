d198c0cd03fb5dd00548e09c8b5af993413ec24e
Tsungyu Yu <ytyubox@gmail.com>
2020-12-16 10:51:54 +0800

    seciton 3

--- Shared/MeetingView.swift
+++ Shared/MeetingView.swift
@@ -14,16 +14,31 @@ struct MeetingView: View {
         VStack {
             ProgressView(value: 5, total: 15)
             HStack {
-                VStack {
+                VStack(alignment: .leading) {
                     Text("Seconds Elapsed")
+                        .font(.caption)
                     Label("300", systemImage: "hourglass.bottomhalf.fill")
                 }
-                VStack {
+                Spacer()
+                VStack(alignment: .trailing) {
                     Text("Seconds Remaining")
+                        .font(.caption)
                     Label("600", systemImage: "hourglass.tophalf.fill")
                 }
             }
+            Circle()
+                .strokeBorder(lineWidth: 24, antialiased: true  )
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button {
+                    
+                } label: {
+                    Image("forward.fill")
+                }
+            }
         }
+        .padding()
     }
 }
 
