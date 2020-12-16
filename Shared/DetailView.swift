76b0f61450b5049825f024d48798e9d1f871e484
Tsungyu Yu <ytyubox@gmail.com>
2020-12-16 15:36:56 +0800

    6-5: can go editView and back

--- Shared/DetailView.swift
+++ Shared/DetailView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct DetailView: View {
     let scrum: DailyScrum
+    @State private var isPresented = false
     var body: some View {
         List {
             Section(header: Text("Meeting Info")) {
@@ -42,7 +43,21 @@ struct DetailView: View {
             }
         }
         .listStyle(InsetGroupedListStyle())
+        .navigationBarItems(trailing: Button("Edit", action: {
+            isPresented = true
+        }))
         .navigationTitle(scrum.title)
+        .fullScreenCover(isPresented: $isPresented, content: {
+            NavigationView {
+                EditView()
+                    .navigationTitle(scrum.title)
+                    .navigationBarItems(leading: Button("Cancel") {
+                        isPresented = false
+                    }, trailing: Button("Done") {
+                        isPresented = false
+                    })
+            }
+        })
     }
 }
 
